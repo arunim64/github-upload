d70a4d016e36527af250eb257e2a9981ee4c9652
arunim64 <75315784+arunim64@users.noreply.github.com>
2020-12-02 10:46:24 +0530

    Add files via upload

---
+++ ProblemSet1Q1.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ProblemSet1Q1 {
+    public static void main(String[] args) {
+        System.out.println("Sum of three numbers");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number 1:");
+        int a = sc.nextInt();
+        System.out.print("Enter number 2:");
+        int b = sc.nextInt();
+        System.out.print("Enter number 3:");
+        int c = sc.nextInt();
+        System.out.print("The sum of these three numbers is:");
+        int sum = a+b+c;
+        System.out.println(sum);
+
+    }
+}
