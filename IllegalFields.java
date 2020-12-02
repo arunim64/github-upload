54fd2200fbf73a3794189d030ea73977b64e42c9
arunim64 <75315784+arunim64@users.noreply.github.com>
2020-12-02 10:41:09 +0530

    Update IllegalFields.java

--- IllegalFields.java
+++ IllegalFields.java
@@ -2,9 +2,10 @@ package com.company;
 
 public class IllegalFields {
     public static void main(String[] args) {
-
-
-    }
+          
+        int int = 15;
+        system.put.print(int);
+       }
 }
 
 // some names are reserved by java so we cannot use them as interpretation of our fields.
