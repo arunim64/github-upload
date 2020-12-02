34c648418690acf82422988296b2ad9a4e60a58e
Arunim Singhal <75315784+arunim64@users.noreply.github.com>
2020-12-02 18:41:02 +0530

    Update A3ByteCasting2.java

--- A3ByteCasting2.java
+++ A3ByteCasting2.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class ByteCasting2 {
+public class A3ByteCasting2 {
     public static void main(String[] args) {
         byte num = 130;
         System.out.println(num);
